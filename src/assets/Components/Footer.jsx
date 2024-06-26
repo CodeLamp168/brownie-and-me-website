27b08acb8fa21c7a407027759dd8af8a1adaf116
Caleb D. Sim <dylansim168@gmail.com>
2024-06-26 17:09:45 +0800

    footer content

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,10 +1,36 @@
 export default function Footer() {
 
-   
+  function FooterDivRight() {
+
+    return (
+        <>
+        <div className="Footer-Right Footer-Div"> 
+            <h2>A Delightful Treat</h2>
+            <p>genericname@gmail.com</p>
+        </div>
+        </>
+    )
+
+   }
+   function FooterDivLeft() {
+
+    return (
+        <>
+        <div className="Footer-Left Footer-Div"> 
+            <ul className="Footer-Social-Display">
+                <img src="/instagram.svg" alt="404" className="instagram svg-logo" />
+                <img src="/messenger.svg" alt="404" className="facebook svg-logo" />
+            </ul>
+        </div>
+        </>
+    )
+
+   }
 
     return(
         <div className="footer-section">
-            footer
+            <FooterDivRight/>
+            <FooterDivLeft/>
         </div>
     )
 }
