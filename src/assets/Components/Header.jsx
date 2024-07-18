cd31284bebdada3edce81389c84e92505492b55f
Caleb D. Sim <dylansim168@gmail.com>
2024-07-18 10:53:07 +0800

    header css placement

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -7,16 +7,29 @@ export default function Header(){
 
         return(
             <div className="logo-box">
-                <div className="logo-img"></div>
+                <div className="logo-img">
+                    <img src="#" alt="404" />
+                </div>
                 <h2 className="logo-name">BROWNIE AND ME</h2>
             </div>
+        )
+    }
+
+    function NavBox(){
+
+        return(
+            <nav className="nav-box">
+                <div className="nav-item">1</div>
+                <div className="nav-item">2</div>
+            </nav>
         )
     }
 
     return(
 
         <section className="header-section layout-section">
-            header-box
+            <LogoBox/>
+            <NavBox/>
         </section>
     )
 }
