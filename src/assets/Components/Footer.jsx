07f9dd04a84d119fdcd16482b34a8b16d2f90887
Caleb D. Sim <dylansim168@gmail.com>
2024-07-18 13:00:47 +0800

    footer content display with css (responsive WIP)

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,10 +1,44 @@
+import instagramSrc from "/instagram.svg"
+import messengerSrc from "/messenger.svg"
 
 
 export default function Footer() {
 
+    const SocialDisplay =({ socialSrc, socialName }) => {
+        return( 
+            <div className={`${socialName}-logo-box social-logo-box`}>
+                <img className="social-logo" src={socialSrc} alt={socialName} />
+            </div>
+        )
+    }
+
+
+    function FooterText() {
+
+        return(
+            <div className="footer-text">
+              <h2>Induldge in delightful treats</h2>
+               <a href="#">johndoe@placeholderemail.com</a>
+            </div>
+        )
+  
+    }
+
+    function SocialBox(){
+        
+        return(
+           <div className="social-box">
+            <SocialDisplay socialSrc={messengerSrc} socialName={'messenger'}/>
+            <SocialDisplay socialSrc={instagramSrc} socialName={'instagram'}/>
+           </div>
+        )
+      
+    }
+
     return(
         <section className="footer-section layout-section">
-        footer-box
+          <FooterText/>
+          <SocialBox/>
         </section>
     )
 }
