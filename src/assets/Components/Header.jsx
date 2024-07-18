0942193a6a4e3e39aff230fbe8a797476f6bde98
Caleb D. Sim <dylansim168@gmail.com>
2024-07-18 10:37:18 +0800

    html header additions

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -3,6 +3,16 @@
 export default function Header(){
 
 
+    function LogoBox(){
+
+        return(
+            <div className="logo-box">
+                <div className="logo-img"></div>
+                <h2 className="logo-name">BROWNIE AND ME</h2>
+            </div>
+        )
+    }
+
     return(
 
         <section className="header-section layout-section">
