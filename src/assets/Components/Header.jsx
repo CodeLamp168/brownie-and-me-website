da1d106cf65e138be198b3dbaea7595ef4592ede
Caleb D. Sim <dylansim168@gmail.com>
2024-07-22 11:56:05 +0800

    added svg for navs

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -2,6 +2,23 @@
 
 export default function Header(){
 
+     function NavBtnOpen(){
+
+        return(
+            <div className="nav-button-open-box">
+                <img className="nav-button-img" src="/nav-menu-open.svg" alt="404" />
+            </div>
+        )
+    }
+
+    function NavBtnClose(){
+
+        return (
+            <div className="nav-button-close-box">
+                <img className="nav-button-display nav-closed-display-style"  src="/nav-menu-close.svg" alt="404" />
+            </div>
+        )
+    }
 
     function LogoBox(){
 
@@ -17,11 +34,16 @@ export default function Header(){
 
     function NavBox(){
 
+
         return(
+            <>
             <nav className="nav-box">
                 <div className="nav-item nav-item-interactive-box"><h3>MENU</h3></div>
                 <div className="nav-item nav-item-interactive-box"><h3>DELIVERY</h3></div>
             </nav>
+
+            <NavBtnClose/>
+            </>
         )
     }
 
@@ -30,6 +52,7 @@ export default function Header(){
         <section className="header-section layout-section">
             <LogoBox/>
             <NavBox/>
+            <NavBtnOpen/>
         </section>
     )
 }
