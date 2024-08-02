140caf86b02b5429037d56bed7db53b9abd87f2e
Caleb D. Sim <dylansim168@gmail.com>
2024-08-02 09:50:38 +0800

    call to action content placeholder

--- src/assets/Components/Main.jsx
+++ src/assets/Components/Main.jsx
@@ -1,9 +1,15 @@
 
-
-
 export default function Main(){
 
 
+    function HeroCTA(){
+        return(
+            <div className="call-to-action">
+                <h2>Make your occasion <i>extra</i> special</h2>
+                <p>Discover our irresistible brownies, cookies, and a delightful array of treats, all made with love and the finest ingredients. Whether you're celebrating a special occasion or simply craving a sweet escape, we've got the perfect goodies to satisfy your every desire. </p>
+            </div>
+        )
+    }
     return(
         <section className="main-section layout-section">
        hero-box
