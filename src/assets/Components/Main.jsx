ff33ae9443dcbf8520c9b41cc9d771e0ad6fdaff
Caleb D. Sim <dylansim168@gmail.com>
2024-08-20 10:12:21 +0800

    wrapped BarDesign and HeroCTA together

--- src/assets/Components/Main.jsx
+++ src/assets/Components/Main.jsx
@@ -23,36 +23,43 @@ export default function Main(){
         )
     }
 
+    function HeroContentWRap(){
+
+        return(
+            <div className="hero-content-wrap"></div>
+        )
+    }
+
 
     function HeroCTA(){
+        
+        const BarDesigns = () => {
+
+            return(
+                <div className="design-bar hero-design-bar"></div>
+            )
+        }
+
+
         return(
+            <>
             <div className="call-to-action">
                 <h2>Make your occasion <i>extra</i> special</h2>
                 <p>Discover our irresistible brownies, cookies, and a delightful array of treats, all made with love and the finest ingredients. Whether you're celebrating a special occasion or simply craving a sweet escape, we've got the perfect goodies to satisfy your every desire. </p>
             </div>
+
+            <BarDesigns/>
+
+            </>
         )
     }
 
-    function HeroImg(){
-        return(
-            <div className="hero-img-wrap">
-                <img src="#" alt="404" className="hero-img" />
-            </div>
-        )
-    }
 
-    function BarDesigns(){
-
-        return(
-            <div className="design-bar hero-design-bar"></div>
-        )
-    }
     return(
         <section className="main-section layout-section">
             
             <HeroGallery/>
             <HeroCTA/>
-            <BarDesigns/>
         </section>
     )
 }
